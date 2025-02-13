40afb5e2fff1f824ad1460961c28e829aaba8854
maxkalina <maks.kalinovskiy.08@mail.ru>
2025-02-13 22:45:35 +0300

    added ex-le of adding model to db, req to extract data

--- backend/app/models/word.py
+++ backend/app/models/word.py
@@ -9,7 +9,33 @@ class WordWithAssociations(SQLModel, table=True):
 
 
 '''
-запрос для модели
+запись в бд
+
+import json
+from sqlmodel import Session, create_engine
+from models import WordWithAssociations
+
+engine = create_engine("sqlite:///words.db")
+SQLModel.metadata.create_all(engine)
+
+with open("../words.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+with Session(engine) as session:
+    for category, words in data.items():
+        for item in words:
+            word_entry = WordWithAssociations(
+                category=category, 
+                word=item["word"],
+                associations=item["associations"]
+            )
+            session.add(word_entry)
+    session.commit()
+
+'''
+
+'''
+запрос на извлечение данных из модели
 
 import json
 from sqlmodel import Session, create_engine
